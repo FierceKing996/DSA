2c5797709c6ced99e069c731eb2463041fe55188
Aditya K Sharma <40303225+FierceKing996@users.noreply.github.com>
2025-08-27 02:41:47 +0530

    Time: 1 ms (100.00%) | Memory: 44.9 MB (20.92%) - LeetSync

---
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode first = null;
+    TreeNode second = null;
+    TreeNode prev = new TreeNode(Integer.MIN_VALUE);
+    
+    public void inorder(TreeNode root){
+        if (root == null) return;
+
+        inorder(root.left);
+
+        // find anomaly
+        if (prev != null && prev.val > root.val) {
+            if (first == null) first = prev;   // first wrong node
+            second = root;                     // second wrong node
+        }
+        prev = root;
+
+        inorder(root.right);
+    }
+    public void recoverTree(TreeNode root) {
+        inorder(root);
+        // swap the two wrong nodes
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+}
